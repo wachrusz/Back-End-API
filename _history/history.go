138e61d9ff37b55ea310165b559c3945c099e67a
Mikhail Vakhrushin <lstwrd@yandex.ru>
2023-11-13 21:43:46 +0300

    Базовая версия API системы для разработки приложения.

---
+++ _history/history.go
@@ -0,0 +1,28 @@
+package history
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func RegisterHandlers(router *mux.Router) {
+	router.HandleFunc("/history/get", GetHistory).Methods("GET")
+	router.HandleFunc("/history/add", AddHistoryEntry).Methods("POST")
+}
+
+func GetHistory(w http.ResponseWriter, r *http.Request) {
+	//TODO: Реализация логики получения истории
+
+	// В данном примере просто возвращаем пустой массив
+	history := []string{}
+	json.NewEncoder(w).Encode(history)
+}
+
+func AddHistoryEntry(w http.ResponseWriter, r *http.Request) {
+	//TODO: Реализация логики добавления записи в историю
+	// В данном примере просто возвращаем успешный ответ
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("History entry added successfully"))
+}
---
+++ _profile/profile.go
@@ -0,0 +1,44 @@
+package profile
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Простой тип для представления профиля пользователя
+type UserProfile struct {
+	Username string `json:"username"`
+	Name     string `json:"name"`
+}
+
+// userProfile - имитация базы данных
+var userProfile = make(map[string]UserProfile)
+
+func RegisterHandlers(router *mux.Router) {
+	router.HandleFunc("/profile/get", GetProfile).Methods("GET")
+	router.HandleFunc("/profile/update", UpdateProfile).Methods("PUT")
+}
+
+func GetProfile(w http.ResponseWriter, r *http.Request) {
+	// Реализация логики получения профиля
+	username := "user1" // В данном примере захардкодим пользователя
+	profile, ok := userProfile[username]
+
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Profile not found"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(profile)
+}
+
+func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+	//TODO: Реализация логики обновления профиля
+
+	// В данном примере просто возвращаем успешный ответ
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Profile updated successfully"))
+}
