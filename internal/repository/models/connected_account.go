56b3c716b7330696209ec7459dc78dc560fd5fd9
Mikhail Vakhrushin <122281679+wachrusz@users.noreply.github.com>
2024-12-13 20:59:49 +0300

    added new field to connected account struct

--- internal/repository/models/connected_account.go
+++ internal/repository/models/connected_account.go
@@ -1,9 +1,10 @@
 package models
 
 type ConnectedAccount struct {
-	ID            string `json:"id"`
-	UserID        string `json:"user_id"`
-	BankID        string `json:"bank_id"`
-	AccountNumber string `json:"account_number"`
-	AccountType   string `json:"account_type"`
+	ID            string  `json:"id"`
+	UserID        string  `json:"user_id"`
+	BankID        string  `json:"bank_id"`
+	AccountNumber string  `json:"account_number"`
+	AccountType   string  `json:"account_type"`
+	AccountState  float64 `json:"account_state"`
 }
